5a73214e7ff941a2b723f36097241bf89a8711f3
Dzhanaev <bindeecode@gmail.com>
2017-11-08 03:00:38 +0300

    Added Exlore Airbnb component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Header/Header";
+import Header from "./Header/Header.js";
+import ExploreAirbnb from "./ExploreAirbnb/ExploreAirbnb";
 
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <div>
+        <Header />
+        <ExploreAirbnb />
+      </div>
+    );
   }
 }
 
